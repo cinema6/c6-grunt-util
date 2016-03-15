1937bc943737cb09ee639077bcf9a03d5530d403
Scott Bouloutian <ScottBouloutian@gmail.com>
2016-03-15 10:53:49 -0400

    wait for command to complete

--- tasks/runcommand.js
+++ tasks/runcommand.js
@@ -4,6 +4,10 @@ var AWS = require('aws-sdk');
 var Q = require('q');
 var path = require('path');
 
+var CHECK_STATUS_WAIT = 5000
+var IN_PROGRESS_STAUSES = ['Pending', 'InProgress', 'Cancelling'];
+var SUCCESS_STATUS = 'Success';
+
 module.exports = function(grunt) {
     var autoscaling;
     var ssm;
@@ -18,7 +22,7 @@ module.exports = function(grunt) {
 
     // Get array of instance ids in an autoscaling group
     function getInstanceIds(group) {
-        return new Q.Promise(function(resolve, reject) {
+        return Q.Promise(function(resolve, reject) {
             autoscaling.describeAutoScalingGroups({
                 AutoScalingGroupNames: [ group ]
             }, function(error, data) {
@@ -35,7 +39,7 @@ module.exports = function(grunt) {
     
     // Run shell command on the specified instance
     function runCommand(instanceIds, command, comment, timeout) {
-        return new Q.Promise(function(resolve, reject) {
+        return Q.Promise(function(resolve, reject) {
             ssm.sendCommand({
                 Comment: comment,
                 DocumentName: 'AWS-RunShellScript',
@@ -54,6 +58,36 @@ module.exports = function(grunt) {
             });
         });
     }
+    
+    // Gets the status of the command
+    function getCommandStatus(id) {
+        return Q.Promise(function(resolve, reject) {
+            ssm.listCommands({
+                CommandId: id
+            }, function(error, data) {
+                if(error) {
+                    reject(error);
+                } else {
+                    resolve(data.Commands[0].Status);
+                }
+            });
+        });
+    }
+    
+    // Resolves once the command has finished executing
+    function waitForCommand(id) {
+        return getCommandStatus(id).then(function(status) {
+            if(IN_PROGRESS_STAUSES.indexOf(status) === -1) {
+                return status;
+            } else {
+                return Q.Promise(function(resolve, reject) {
+                    setTimeout(function() {
+                        return waitForCommand(id).then(resolve, reject);
+                    }, CHECK_STATUS_WAIT);
+                });
+            }
+        });
+    }
 
     grunt.registerTask('runcommand', 'runs an EC2 runcommand against an instance or autoscaling group', function() {
         var done = this.async();
@@ -64,10 +98,10 @@ module.exports = function(grunt) {
         var comment = grunt.option('comment') || '';
         var instanceList = grunt.option('instances');
         var region = grunt.option('region') || 'us-east-1';
-        var timeout = grunt.option('execTimeout') || 10000;
+        var timeout = grunt.option('execTimeout') || 30000;
 
         initAWS(auth, region);
-        
+
         if(!command) {
             grunt.log.writeln('Must specify a command to run.');
             done(false);
@@ -84,17 +118,26 @@ module.exports = function(grunt) {
         if(instanceList) {
             instances.concat(instanceList.split(','));
         }
-        return Q.resolve(function() {
+        return Q.resolve().then(function() {
             if(autoscalingGroup) {
+                grunt.log.writeln('Looking up instances in autoscaling group');
                 return getInstanceIds(autoscalingGroup).then(function(ids) {
-                    instances.concat(ids);
+                    instances = instances.concat(ids);
                 });
             }
         }).then(function() {
+            grunt.log.writeln('Running command on instances ' + instances);
             return runCommand(instances, command, comment, Math.round(timeout / 1000));
-        }).then(function() {
-            grunt.log.writeln('Finished running the command');
-            done();
+        }).then(function(data) {
+            grunt.log.writeln('Waiting for command to complete');
+            return waitForCommand(data.Command.CommandId);
+        }).then(function(status) {
+            if(status === SUCCESS_STATUS) {
+                grunt.log.writeln('Finished running the command');
+                done();
+            } else {
+                return Q.reject('Command failed with status ' + status);
+            }
         }).catch(function(error) {
             grunt.log.error(error);
             done(false);
